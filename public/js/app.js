c8537cd18084b3ca27d690859a68edfcce6de188
jfkbeatkid <hernadezrigoberto@gmail.com>
2016-06-29 21:01:19 -0400

    finshed color cycle

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,10 @@ var container = $('.container');
 var numOfRows = $('#Rows')
 var numOfCols = $('#Columns')
 var submit = $('#submit')
+var colorClasses = ['red', 'green', 'blue', 'white'];
+var colorCycle = colorClasses.length-4;
+var previousCell;
+
 
   submit.on('click', initGrid);
   // addClickHandlers();
@@ -17,17 +21,26 @@ var submit = $('#submit')
   function testFunction(){
     console.log(numOfRows);
     console.log(numOfCols);
+
   }
 
   function changeColor(){
-    var colorClasses = ['white', 'red', 'green', 'blue'];
-    var colorCycle = Math.round(Math.random() * colorClasses.length);
-    console.log(colorCycle);
-    var color = colorClasses[colorCycle];
-    $(this).removeClass(colorClasses.join(' '));
-    $(this).addClass(color);
-    colorCycle += 1;
+  if(previousCell === this){
+      // var color = ['white', 'red', 'green', 'blue'];
+      var color = colorClasses[colorCycle];
+      if(colorCycle > 3){
+        colorCycle = 0
+      } else {
+      $(this).removeClass(colorClasses.join(' '));
+      $(this).addClass(color);
+      colorCycle += 1;
+      console.log(colorCycle);
+    }
+  }else {
+    colorCycle = 0;
+    previousCell = this;
   }
+};
   // function addClickHandlers(){
   //   var cells = $('.cell');
   //   for(var counter = 0; counter < cells.length; counter += 1){
@@ -36,6 +49,9 @@ var submit = $('#submit')
   //   }
   // }
   function initGrid(){
+      $('.container').empty();
+      // submit.addClass('disabled');
+      // submit.attr('disabled', true);
       for(var i = 0; i < numOfRows.val(); i += 1){
         var row = $('<div></div>');
         row.addClass('row');
